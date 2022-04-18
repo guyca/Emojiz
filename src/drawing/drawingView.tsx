ef008075d91c1473fe05f35df5a849a7fb772d84
Guy Carmeli <guyc@wix.com>
2022-04-18 20:43:40 +0300

    Simplify DrawingView class

--- src/drawing/drawingView.tsx
+++ src/drawing/drawingView.tsx
@@ -7,8 +7,8 @@ import {
   useTouchHandler,
 } from '@shopify/react-native-skia';
 import {observer} from 'mobx-react-lite';
-import React, {useCallback} from 'react';
-import {StyleSheet, Dimensions, View} from 'react-native';
+import React from 'react';
+import {StyleSheet, Dimensions} from 'react-native';
 import {Canvas} from './canvas';
 
 const window = Dimensions.get('window');
@@ -23,49 +23,21 @@ interface Props {
 }
 
 export const DrawingView = observer(({canvas}: Props) => {
-  const onDrawingStart = useCallback(
-    (touchInfo: TouchInfo) => {
-      canvas.onDrawingStart(touchInfo);
-    },
-    [canvas],
-  );
-
-  const onDrawingActive = useCallback(
-    (touchInfo: TouchInfo) => {
-      canvas.onDrawingActive(touchInfo);
-    },
-    [canvas],
-  );
-
-  const onDrawingFinished = useCallback(
-    (touchInfo: TouchInfo) => {
-      canvas.onDrawingFinished(touchInfo);
-    },
-    [canvas],
-  );
-
   const touchHandler = useTouchHandler(
     {
-      onStart: onDrawingStart,
-      onActive: onDrawingActive,
-      onEnd: onDrawingFinished,
+      onStart: (touchInfo: TouchInfo) => canvas.onDrawingStart(touchInfo),
+      onActive: (touchInfo: TouchInfo) => canvas.onDrawingActive(touchInfo),
+      onEnd: (touchInfo: TouchInfo) => canvas.onDrawingFinished(touchInfo),
     },
     [],
   );
 
-  const onDraw = useDrawCallback(
-    (skiaCanvas, info) => {
-      touchHandler(info.touches);
-      skiaCanvas.drawPath(canvas.pathToDraw, textPaint);
-    },
-    [canvas.strokes],
-  );
+  const onDraw = useDrawCallback((skiaCanvas, info) => {
+    touchHandler(info.touches);
+    skiaCanvas.drawPath(canvas.pathToDraw, textPaint);
+  }, []);
 
-  return (
-    <View>
-      <SkiaView style={styles.skiaView} onDraw={onDraw} />
-    </View>
-  );
+  return <SkiaView style={styles.skiaView} onDraw={onDraw} />;
 });
 
 const styles = StyleSheet.create({
